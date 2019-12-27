555ff3781b6b972a51ffa621b93fac11c8d44add
Yelyzaveta <elizaveta.kzm@gmail.com>
2019-12-27 17:39:34 +0100

    Updates

--- precache-manifest.147f6effc2384298d49415cbcfcd222a.js
+++ precache-manifest.147f6effc2384298d49415cbcfcd222a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46569d56ce64fdfc5a26c5e450f12f5e",
+    "revision": "0bfac4a7faccd6e008b6542e9a41bfab",
     "url": "/tasks-organizer/index.html"
   },
   {
-    "revision": "ecb1cdedc1a582d8b2c7",
+    "revision": "5d1c3979a83feee6ec06",
     "url": "/tasks-organizer/static/css/main.68468fa7.chunk.css"
   },
   {
-    "revision": "2ab96263677bec614e02",
-    "url": "/tasks-organizer/static/js/2.27091f22.chunk.js"
+    "revision": "582ee508da79732c2c42",
+    "url": "/tasks-organizer/static/js/2.4ef22fb6.chunk.js"
   },
   {
     "revision": "ca2ac7f480ee25b60e7e93406c94ee2c",
-    "url": "/tasks-organizer/static/js/2.27091f22.chunk.js.LICENSE"
+    "url": "/tasks-organizer/static/js/2.4ef22fb6.chunk.js.LICENSE"
   },
   {
-    "revision": "ecb1cdedc1a582d8b2c7",
-    "url": "/tasks-organizer/static/js/main.03f3d21a.chunk.js"
+    "revision": "5d1c3979a83feee6ec06",
+    "url": "/tasks-organizer/static/js/main.a6717a1d.chunk.js"
   },
   {
     "revision": "285136fbbf3e1de709d2",
